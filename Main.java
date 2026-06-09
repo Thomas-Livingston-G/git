d516b5d2c877410d2cc4b44bad5b7cdd2e45a32e
thomas <thomaslivingston31306@gmail.com>
2026-06-10 01:51:57 +0530

    second commit

--- Main.java
+++ Main.java
@@ -4,5 +4,6 @@ public class Main{
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.print("thomas feature");
+        System.out.println("fetaure 2.0");
     }
 }
